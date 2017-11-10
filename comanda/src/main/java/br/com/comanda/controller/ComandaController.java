cc54bbedcac51dc333da4d7a8ea507e876fb8638
Suporte <Suporte@Suporte-PC>
2017-11-10 12:19:11 -0200

    Atualização do Página de comanda-cadastro e controller

--- comanda/src/main/java/br/com/comanda/controller/ComandaController.java
+++ comanda/src/main/java/br/com/comanda/controller/ComandaController.java
@@ -1,22 +1,30 @@
 package br.com.comanda.controller;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.comanda.dao.ClienteDAO;
 import br.com.comanda.dao.ComandaDAO;
+import br.com.comanda.dto.Cliente;
 import br.com.comanda.dto.Comanda;
+import br.com.comanda.dto.TipoComanda;
 
 @Controller
-@RequestMapping("/abrir")
 public class ComandaController {
 	
 	@Autowired
 	private ComandaDAO comandaDAO;
 	
-	@RequestMapping(value = {"/mesa/{numeroComanda}/status/{status}/id/{id}"})
+	@Autowired
+	private ClienteDAO clienteDAO;
+	
+	@RequestMapping(value = {"/abrir/mesa/{numeroComanda}/status/{status}/id/{id}"})
 	public ModelAndView abrirMesa(@PathVariable("numeroComanda") int numeroComanda, @PathVariable("status")boolean status, @PathVariable("id")Long id) {
 		
 		Comanda comanda;
@@ -29,21 +37,29 @@ public class ComandaController {
 			return mv;
 			
 		}else {
+			
 			comanda = new Comanda();
+			comanda.setHoraAbertura(new Date());
 			comanda.setNumeroComanda(numeroComanda);
 			comanda.setStatus(true);
 			
+			TipoComanda tipoComanda = new TipoComanda();
+			tipoComanda.setId(1);
+			
+			comanda.setTipo(tipoComanda);
+			
 			ModelAndView mv = new ModelAndView("index");
 			mv.addObject("UserClickComandaCadastro", true);
+			mv.addObject("comanda", comanda);
 			return mv;
-		}
-		
-		
-		
-		
-		
-		
+		}	
 		
 	}
+	
+	public List<Cliente> listarClientes(){
+		
+		return clienteDAO.listar();
+	}
+	
 
 }
--- comanda/src/main/webapp/WEB-INF/views/comanda-cadastro.jsp
+++ comanda/src/main/webapp/WEB-INF/views/comanda-cadastro.jsp
@@ -1,9 +1,59 @@
-
+<%@taglib prefix="sf" uri="http://www.springframework.org/tags/form"%>
 <div id='content'>
 	<div class="container">
 		<div class="row">
-		
 			<h1>Comanda Cadastro</h1>
+
+			<div class="row">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<div class="col-xs-12">
+							<h3>Abertura de Mesa</h3>
+						</div>
+					</div>
+					<div class="panel-body">
+						<div>
+							<form action="${contextRoot}/comanda/salvarcomanda" method="POST"
+								class="form-horizontal">
+
+
+								<div class="col-xs-12 form-group">
+									<div class="col-xs-12">
+										<label for="mesa">Mesa</label>
+									</div>
+									<div class=" col-xs-3">
+										<input name="mesa" id="mesa" class="form-control"
+											value="${numeroComanda}" disabled="disabled" />
+									</div>
+								</div>
+
+								<div class="col-xs-12">
+									<div class="form-group col-md-3 row">
+										<label for="cliente">Cliente</label> <input name="cliente"
+											id="cliente" class="form-control" value="Venda ao Consumidor" />
+									</div>
+								</div>
+								<!-- 
+								<div class="col-xs-12">
+									<div class="form-group col-md-3 row">
+										<label for="atendente">Atendente</label> <input
+											name="atendente" id="atendente" class="form-control" />
+									</div>
+								</div>
+								 -->
+								<div class="col-xs-12">
+									<input type="submit" class="btn btn-success" value="Próximo" />
+									<a href="${contextRoot}/comanda" class="btn btn-danger">Sair</a>
+								</div>
+							</form>
+						</div>
+					</div>
+
+
+
+				</div>
+			</div>
+
 		</div>
 	</div>
 </div>
